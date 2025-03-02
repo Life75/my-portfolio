933eb7216e40dbe72cd4ad288c0a308870de78a7
Life75 <austynwashington@yahoo.com>
2025-03-02 17:29:54 -0500

    setup daisyui configs

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <template>
+      <button class=" btn-primary">Hello World</button>
+
+    </template>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  css: ["~/assets/css/main.css"],
+  compatibilityDate: "2025-03-02",
+});
